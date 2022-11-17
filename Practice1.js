ee90ec9bd2909eebb4c45b0dbb83741b446457a4
shivani awasthi <shvaniawsthi001@gmail.com.com>
2022-11-17 12:24:33 +0530

    obj

---
+++ Practice1.js
@@ -0,0 +1,31 @@
+// craete a variable of type string and try to add no. to it.
+ let a = "shivani"
+ let b =6
+ console.log(a+b)
+
+//  2=>use typeof operator to find the datatype of the str in 
+// last qstn.
+
+console.log(typeof(a+b))
+// 3=> create a const obj in js .can you change it 
+// to hold a number later?
+
+const d ={
+        "name":"shivani"
+}
+
+// no we cant
+
+// 4=>try to add a new key to the const obj
+//  in prbm 3 .were you able to do it?
+
+// we can add new keys and change the key values in obj
+
+d["class"]=7;
+d["name"]= "shraddha"
+console.log(d)
+
+
+
+
+
